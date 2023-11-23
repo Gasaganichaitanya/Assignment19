b2d67ceae7b01d966d0ff9a7b5ac4f8be4850f77
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-11-23 15:56:11 +0530

    initial commit

---
+++ ShapeAreaLib/ShapeAreaLib/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaLib
+{
+    public class Circle
+    {
+        public double Area(double r)
+        {
+            double area = 3.14 * r * r;
+            return area;
+        }
+    }
+}
